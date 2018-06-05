4c13375ea7040f59710056c46f54ac6719fb6e0d
ojdduran <omardurandiaz@gmail.com>
2018-06-04 22:41:33 -0400

    Segunda Subida

--- sisVentas/resources/views/almacen/categoria/index.blade.php
+++ sisVentas/resources/views/almacen/categoria/index.blade.php
@@ -2,7 +2,7 @@
 @section ('contenido')
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-		<h3>Listado de Categorías <a href="categoria/create"><button class="btn btn-success">Nuevo</button></a></h3>
+		<h3>Listado de Categorías <a href="categoria/create"><button class="btn btn-success">Nuevo</button class="btn btn-success"></a></h3>
 		@include('almacen.categoria.search')
 	</div>
 </div>
